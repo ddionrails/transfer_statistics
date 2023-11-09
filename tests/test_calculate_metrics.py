f71617b10fcd132000383d0403801529b461a02a
Dominique Hansen <DominiqueHansen@gmx.net>
2023-11-09 14:21:22 +0100

    Format code

--- tests/test_calculate_metrics.py
+++ tests/test_calculate_metrics.py
@@ -2,7 +2,11 @@ from unittest import TestCase
 
 from numpy import array
 
-from transfer_statistics.calculate_metrics import weighted_median, weighted_mean_and_confidence_interval, weighted_boxplot_sections
+from transfer_statistics.calculate_metrics import (
+    weighted_median,
+    weighted_mean_and_confidence_interval,
+    weighted_boxplot_sections,
+)
 
 
 class test_calculate_metrics(TestCase):
@@ -10,13 +14,19 @@ class test_calculate_metrics(TestCase):
         values = array([1, 2, 3, 4, 5])
         weights = array([1, 1, 1, 1, 1])
         expected_median = 3
-        self.assertEqual(expected_median, weighted_median(values=values, weights=weights))
+        self.assertEqual(
+            expected_median, weighted_median(values=values, weights=weights)
+        )
         weights = array([2, 1, 1, 1, 1])
         expected_median = 2.5
-        self.assertEqual(expected_median, weighted_median(values=values, weights=weights))
+        self.assertEqual(
+            expected_median, weighted_median(values=values, weights=weights)
+        )
         weights = array([3, 1, 1, 1, 1])
         expected_median = 2
-        self.assertEqual(expected_median, weighted_median(values=values, weights=weights))
+        self.assertEqual(
+            expected_median, weighted_median(values=values, weights=weights)
+        )
 
     def test_weighted_mean_and_confidence_interval(self):
         values = array([1, 2, 3, 4, 5])
