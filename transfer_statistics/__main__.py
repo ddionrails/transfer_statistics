fedce72dbcd5b3d1ea76562fe65df374d67f7bbb
Dominique Hansen <DominiqueHansen@gmx.net>
2023-11-17 14:17:11 +0100

    Remove missings before aggregation

--- transfer_statistics/__main__.py
+++ transfer_statistics/__main__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pandas import DataFrame, Series
+from numpy import isnan
 
 
 from transfer_statistics.types import VariableMetadata
@@ -34,6 +35,9 @@ def _apply_numeric_aggregations(
     grouped_data_frame = grouped_data_frame.sort_values(by=variable_name)
     values = grouped_data_frame[variable_name].to_numpy()
     weights = grouped_data_frame["weights"].to_numpy()
+    # TODO: What to do with Missings?
+    weights = weights[~isnan(values)]
+    values = values[~isnan(values)]
     output = weighted_mean_and_confidence_interval(values, weights)
     output = output | weighted_boxplot_sections(values, weights)
     output = output | bootstrap_median(values, weights)
