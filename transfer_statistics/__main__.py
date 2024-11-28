4b90f1a2d1b0f97f0be0c9a1b0fa0aa339b7f36e
Dominique Hansen <DominiqueHansen@gmx.net>
2024-11-28 17:12:29 +0100

    Fix generation of scrambled dataframes

--- transfer_statistics/__main__.py
+++ transfer_statistics/__main__.py
@@ -121,7 +121,10 @@ def cli():
         _type="categorical",
     )
     handle_statistics(
-        data=data,
+        # Without this some dataframes for variables without groupings
+        # are merged wrong.
+        # TODO: Maybe this could be fixed with initializing an empty df and appending results
+        data=data.replace(MISSING_VALUES, NaN),
         metadata=metadata,
         all_value_labels=all_value_labels,
         output_folder=numerical_output_path,
